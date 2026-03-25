261572c23bfb03aed556344f63421ef5b54ec1ff
Amandella2020 <virgynnadi@gmail.com>
2026-03-25 11:48:59 +0100

    update

--- shopcare/settings.py
+++ shopcare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u&6567!s%)wrsl5qmd5llgj)p#p$y0vds72&=8b-787n_)g@-a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
